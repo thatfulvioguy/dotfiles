07e539795ee8fbaa1669138981aebe6970845634
Fulvio Valente <2515227+thatfulvioguy@users.noreply.github.com>
2020-11-15 21:11:48 +0000

    Add bobthefish theme settings

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+
+# bobthefish settings
+
+set -g theme_display_date no
+set -g theme_display_vi no
+set -g theme_display_ruby no
+set -g theme_display_vagrant no
+set -g theme_display_hg no
+
+set -g theme_title_display_process yes
+set -g theme_display_k8s_context yes
+set -g theme_display_k8s_namespace yes
