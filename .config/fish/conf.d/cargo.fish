0af25bae7881d3666887201d126899078f8d96ba
Fulvio Valente <2515227+thatfulvioguy@users.noreply.github.com>
2020-11-22 02:42:07 +0000

    Add cargo and other rust tooling to $fish_user_paths

---
+++ .config/fish/conf.d/cargo.fish
@@ -0,0 +1,4 @@
+
+# This is typically done by setting it once as a universal var instead of on startup, but that
+# means having to remember to do it on any new machine. This is easy to sync.
+set fish_user_paths $HOME/.cargo/bin $fish_user_paths
