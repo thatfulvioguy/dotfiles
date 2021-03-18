50af214e21ea41dfc88936c60a4f107b0db9dde3
Fulvio Valente <2515227+thatfulvioguy@users.noreply.github.com>
2021-03-18 17:19:52 +0000

    Remove adding cargo to fish_user_paths - even without `set -U`, it'll still add it to the universal var each time the shell's started...

--- .config/fish/conf.d/cargo.fish
+++
@@ -1,4 +0,0 @@
-
-# This is typically done by setting it once as a universal var instead of on startup, but that
-# means having to remember to do it on any new machine. This is easy to sync.
-set fish_user_paths $HOME/.cargo/bin $fish_user_paths
