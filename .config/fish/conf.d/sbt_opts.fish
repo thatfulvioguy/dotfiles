539ac62c5ee2ce265c9e0a3f1734303561b21e05
Fulvio Valente <2515227+thatfulvioguy@users.noreply.github.com>
2023-04-27 12:06:26 +0100

    Don't set SBT_OPTS in fish config, prefer doing it in `~/.config/environ.ent.d`

--- .config/fish/conf.d/sbt_opts.fish
+++
@@ -1,6 +0,0 @@
-
-set -x SBT_OPTS "-Xmx2048m \
--XX:ReservedCodeCacheSize=256m \
--XX:MaxHeapFreeRatio=30 -XX:MinHeapFreeRatio=10 \
--XX:MaxInlineLevel=20 \
---add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED"
